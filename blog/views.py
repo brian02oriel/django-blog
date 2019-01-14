3b990dccc0545866074bfdf1ed2e3b8af39a7bd3
brian02oriel <brian.nieto1@utp.ac.pa>
2019-01-14 12:20:45 -0500

    No inserta los datos en la db. No muestra los datos en 'home'.

--- blog/views.py
+++ blog/views.py
@@ -11,18 +11,32 @@ def index(request):
     return render(request, 'blog/index.html')
 
 def home(request):
-    return render(request, 'blog/home.html')
+    comments = Users.objects.all()
+    for comment in comments:
+        fullname = comment.fullname
+        email = comment.email
+        message = comment.message
+        pub_date = comment.pub_date
+
+    context = {
+        "fullname":fullname,
+        "email":email,
+        "message":message,
+        "pub_date":pub_date
+    }
+    return render(request, 'blog/home.html', context)
 
 def contact(request):
     contactform = ContactForm(request.POST or None)
     pub_date = timezone.now()
+    
     if contactform.is_valid():
         data = contactform.cleaned_data
+        print(data)
         db_register = Users(
             fullname = data.get("fullname"),
             email = data.get("email"),
             message = data.get("message"),
             pub_date = pub_date )
         db_register.save()
-        
     return render(request, 'blog/contact.html', {'form':contactform})
