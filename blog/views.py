82dfe34150a4ed473502b00eb646d4991a4b7b9f
brian02oriel <brian.nieto1@utp.ac.pa>
2019-01-14 12:24:35 -0500

    Inserta los datos en la db, pero no los muestra en 'home'

--- blog/views.py
+++ blog/views.py
@@ -11,6 +11,20 @@ def index(request):
     return render(request, 'blog/index.html')
 
 def home(request):
+    #Inserta los datos del formulario en la db
+    pub_date = timezone.now()
+    contactform = ContactForm(request.POST or None)
+    if contactform.is_valid():
+        data = contactform.cleaned_data
+        print(data)
+        db_register = Users(
+            fullname = data.get("fullname"),
+            email = data.get("email"),
+            message = data.get("message"),
+            pub_date = pub_date )
+        db_register.save()
+    
+    #Selecciona los datos para mostrarlos en pantalla
     comments = Users.objects.all()
     for comment in comments:
         fullname = comment.fullname
@@ -28,15 +42,5 @@ def home(request):
 
 def contact(request):
     contactform = ContactForm(request.POST or None)
-    pub_date = timezone.now()
     
-    if contactform.is_valid():
-        data = contactform.cleaned_data
-        print(data)
-        db_register = Users(
-            fullname = data.get("fullname"),
-            email = data.get("email"),
-            message = data.get("message"),
-            pub_date = pub_date )
-        db_register.save()
     return render(request, 'blog/contact.html', {'form':contactform})
