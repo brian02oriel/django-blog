a4bd5ef42db74ac0c373867ffe3af09ae73fdb7c
brian02oriel <brian.nieto1@utp.ac.pa>
2019-01-15 23:47:48 -0500

    Guarda y redirecciona, mas no muestra la lista

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.utils import timezone
+from django.urls import reverse
 
 from .models import Users
 from .forms import ContactForm
@@ -11,18 +12,6 @@ def index(request):
     return render(request, 'blog/index.html')
 
 def home(request):
-    #Inserta los datos del formulario en la db
-    pub_date = timezone.now()
-    contactform = ContactForm(request.POST or None)
-    if contactform.is_valid():
-        data = contactform.cleaned_data
-        print(data)
-        db_register = Users(
-            fullname = data.get("fullname"),
-            email = data.get("email"),
-            message = data.get("message"),
-            pub_date = pub_date )
-        db_register.save()
     
     #Selecciona los datos para mostrarlos en pantalla
     comments = Users.objects.all()
@@ -41,6 +30,19 @@ def home(request):
     return render(request, 'blog/home.html', context)
 
 def contact(request):
-    contactform = ContactForm(request.POST or None)
-    
-    return render(request, 'blog/contact.html', {'form':contactform})
+    #Inserta los datos del formulario en la db
+    pub_date = timezone.now()
+    contactform = ContactForm(request.POST)
+    if request.method == 'POST':
+        if contactform.is_valid():
+            data = contactform.cleaned_data
+            #print(data)
+            db_register = Users(
+                fullname = data.get("fullname"),
+                email = data.get("email"),
+                message = data.get("message"),
+                pub_date = pub_date )
+            db_register.save()
+            return HttpResponseRedirect(reverse('blog:home'))
+    else:
+        return render(request, 'blog/contact.html', {'form':contactform})
